6b6ad5754e27fd8aedb1e9f025427ae90b77b592
Gnana Priya <gnanapriya.namasivayam@syncfusion.com>
2018-12-12 13:01:30 +0530

    Removed unwanted codes from sample.

---
+++ TriggersInListView/TriggersInListView/TriggersInListView/Model/Contacts.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListviewSample
+{
+    public class Contacts : INotifyPropertyChanged
+    {
+        private string contactName;
+        private int contactNumber;
+        private bool favorite;
+
+        public Contacts(string name, int number)
+        {
+            contactName = name;
+            contactNumber = number;
+        }
+
+        public Contacts()
+        {
+
+        }
+
+        public bool Favorite
+        {
+            get { return favorite; }
+            set
+            {
+                if (favorite != value)
+                {
+                    favorite = value;
+                    this.RaisedOnPropertyChanged("Favorite");
+                }
+            }
+        }
+        public string ContactName
+        {
+            get { return contactName; }
+            set
+            {
+                if (contactName != value)
+                {
+                    contactName = value;
+                    this.RaisedOnPropertyChanged("ContactName");
+                }
+            }
+        }
+
+        public Int32 ContactNumber
+        {
+            get { return contactNumber; }
+            set
+            {
+                if (contactNumber != value)
+                {
+                    contactNumber = value;
+                    this.RaisedOnPropertyChanged("ContactNumber");
+                }
+            }
+        }
+
+      
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+    }
+}
